f0571a51f9174733427bb2b2206dc261ffa5042b
brenobarbareli <breno_barbareli@hotmail.com>
2022-06-22 20:47:37 -0300

    day 2

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,14 +1,24 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, useSession } from 'next-auth/react';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
+  const { data: session } = useSession();
+
+  console.log(session);
+
+  console.log(process.env.NEXT_PUBLIC_STRIPE_API_KEY);
+
+  return session ? (
     <button type='button' className={styles.signInButton}>
       <FaGithub color='#04d361' />
       Breno Barbareli
-      <FiX color='#737380' className={styles.closeIcon} />
+      <FiX
+        color='#737380'
+        className={styles.closeIcon}
+        onClick={() => signIn('github')}
+      />
     </button>
   ) : (
     <button type='button' className={styles.signInButton}>
